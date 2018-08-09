6e2fe16d49afa1cc268081628c5d07d0a0c922f5
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-08-09 16:02:44 -0500

    probando traduccion a ingles

--- js/site.js
+++ js/site.js
@@ -1,4 +1,44 @@
+/*2d array of languages*/
+var aLangKeys=new Array();
+aLangKeys['en']=new Array();
+aLangKeys['es']=new Array();
+/*words */
+aLangKeys['es']['home']="Inicio";
+aLangKeys['es']['news']="Noticias";
+aLangKeys['es']['products']="Productos";
+aLangKeys['es']['log-in']="Ingresar";
+aLangKeys['es']['sign-in']="Registro";
+aLangKeys['es']['ollie-keyboard-ad']="Ya puedes llevar a Ollie contigo a todos lados!";
+aLangKeys['es']['see-more']="Ver más >";
+aLangKeys['es']['read-more']="Leer más »";
+aLangKeys['es']['copyright']="© 2016 - 2018 Virtual Robot®. Biowiza S.A. Todos los derechos reservados. Costa Rica. Puedes revisar nuestros";
+aLangKeys['es']['legal-terms']="Términos de Uso";
+aLangKeys['es']['legal-policy']="Política de Privacidad";
+
+aLangKeys['en']['home']="Home";
+aLangKeys['en']['news']="News";
+aLangKeys['en']['products']="Products";
+aLangKeys['en']['log-in']="Login";
+aLangKeys['en']['sign-in']="Signin";
+aLangKeys['en']['ollie-keyboard-ad']="Now you can take Ollie by everywhere!";
+aLangKeys['en']['see-more']="See More >";
+aLangKeys['en']['read-more']="Read More »";
+aLangKeys['en']['copyright']="© 2016 - 2018 Virtual Robot®. Biowiza S.A. All Rights Reserved. Costa Rica. You can check our";
+aLangKeys['en']['legal-terms']="Terms of Use";
+aLangKeys['en']['legal-policy']="Privacy Policy";
+
 $(document).ready(function () {
+
+    /*Testeando traduccion */
+    $('.lang').click(function () {
+        var lang = $(this).attr('id'); // obtain language id
+        // translate all translatable elements
+        $('.tr').each(function (i) {
+            $(this).text(aLangKeys[lang][$(this).attr('key')]);
+        });
+    });
+        
+
 	$('.carousel').carousel({
 		interval: 3000
 	})
