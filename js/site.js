f517efff4562cbcc2d19c686fac55f9babaf6f3f
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-08-10 14:20:51 -0500

    cambio del idioma usandio ipfi para obtener ip e ipstack para obtener resto de la informacion

--- js/site.js
+++ js/site.js
@@ -1,44 +1,72 @@
-/*2d array of languages*/
-var aLangKeys=new Array();
-aLangKeys['en']=new Array();
-aLangKeys['es']=new Array();
-/*words */
-aLangKeys['es']['home']="Inicio";
-aLangKeys['es']['news']="Noticias";
-aLangKeys['es']['products']="Productos";
-aLangKeys['es']['log-in']="Ingresar";
-aLangKeys['es']['sign-in']="Registro";
-aLangKeys['es']['ollie-keyboard-ad']="Ya puedes llevar a Ollie contigo a todos lados!";
-aLangKeys['es']['see-more']="Ver más >";
-aLangKeys['es']['read-more']="Leer más »";
-aLangKeys['es']['copyright']="© 2016 - 2018 Virtual Robot®. Biowiza S.A. Todos los derechos reservados. Costa Rica. Puedes revisar nuestros";
-aLangKeys['es']['legal-terms']="Términos de Uso";
-aLangKeys['es']['and']="y";
-aLangKeys['es']['legal-policy']="Política de Privacidad";
-
-aLangKeys['en']['home']="Home";
-aLangKeys['en']['news']="News";
-aLangKeys['en']['products']="Products";
-aLangKeys['en']['log-in']="Login";
-aLangKeys['en']['sign-in']="Signin";
-aLangKeys['en']['ollie-keyboard-ad']="Now you can take Ollie by everywhere!";
-aLangKeys['en']['see-more']="See More >";
-aLangKeys['en']['read-more']="Read More »";
-aLangKeys['en']['copyright']="© 2016 - 2018 Virtual Robot®. Biowiza S.A. All Rights Reserved. Costa Rica. You can check our";
-aLangKeys['en']['legal-terms']="Terms of Use";
-aLangKeys['en']['and']="and";
-aLangKeys['en']['legal-policy']="Privacy Policy";
-
 $(document).ready(function () {
+        
+    /*2d array of languages*/
+        var aLangKeys = new Array();
+        aLangKeys['en'] = new Array();
+        aLangKeys['es'] = new Array();
+        /*words */
+        aLangKeys['es']['home'] = "Inicio";
+        aLangKeys['es']['news'] = "Noticias";
+        aLangKeys['es']['products'] = "Productos";
+        aLangKeys['es']['log-in'] = "Ingresar";
+        aLangKeys['es']['sign-in'] = "Registro";
+        aLangKeys['es']['ollie-keyboard-ad'] = "Ya puedes llevar a Ollie contigo a todos lados!";
+        aLangKeys['es']['see-more'] = "Ver más >";
+        aLangKeys['es']['read-more'] = "Leer más »";
+        aLangKeys['es']['copyright'] = "© 2016 - 2018 Virtual Robot®. Biowiza S.A. Todos los derechos reservados. Costa Rica. Puedes revisar nuestros";
+        aLangKeys['es']['legal-terms'] = "Términos de Uso";
+        aLangKeys['es']['and'] = "y";
+        aLangKeys['es']['legal-policy'] = "Política de Privacidad";
+    
+        aLangKeys['en']['home'] = "Home";
+        aLangKeys['en']['news'] = "News";
+        aLangKeys['en']['products'] = "Products";
+        aLangKeys['en']['log-in'] = "Login";
+        aLangKeys['en']['sign-in'] = "Signin";
+        aLangKeys['en']['ollie-keyboard-ad'] = "Now you can take Ollie by everywhere!";
+        aLangKeys['en']['see-more'] = "See More >";
+        aLangKeys['en']['read-more'] = "Read More »";
+        aLangKeys['en']['copyright'] = "© 2016 - 2018 Virtual Robot®. Biowiza S.A. All Rights Reserved. Costa Rica. You can check our";
+        aLangKeys['en']['legal-terms'] = "Terms of Use";
+        aLangKeys['en']['and'] = "and";
+        aLangKeys['en']['legal-policy'] = "Privacy Policy";
+      
+        const spanis_lc = "es";
+        const english_lc = "en";
+        curr_lang = spanis_lc;
+        var public_ip;
+    /*ipfi */
+    $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
+        function (json) {
+            public_ip = json.ip;
+            console.log(json.ip);
+            /*ipstack pasando ip publica obtenida y api key */
+            $.getJSON('http://api.ipstack.com/' + public_ip + '?access_key=74eda19312e616de81089ad81ac39014', function (data) {
+ 
+                var language_code = data.location.languages[0].code;
+                console.log("codigo lenguaje", language_code);
+                //estructura de decision para decidir el id del idioma
+                if (language_code != spanis_lc) {
+                    curr_lang = english_lc;
+                }
+                console.log("current language", curr_lang);
+                /*cambio del idioma */
+                $('.tr').each(function (i) {
+                    $(this).text(aLangKeys[curr_lang][$(this).attr('key')]);
+                });
+    
+            });
+        }
+    );
 
     /*Testeando traduccion */
-    $('.lang').click(function () {
+    /*$('.lang').click(function () {
         var lang = $(this).attr('id'); // obtain language id
         // translate all translatable elements
         $('.tr').each(function (i) {
             $(this).text(aLangKeys[lang][$(this).attr('key')]);
         });
-    });
+    });*/
         
 
 	$('.carousel').carousel({
