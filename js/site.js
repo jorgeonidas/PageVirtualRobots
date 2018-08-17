82ed3ed6063387ef5f0199539cd97968a4f282a6
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-08-17 09:28:12 -0500

    traduccion politica de privacidad

--- js/site.js
+++ js/site.js
@@ -152,22 +152,51 @@ $(document).ready(function () {
         aLangKeys['en']['pp6'] = "6. Information about children";
         aLangKeys['en']['pp7'] = "7. Changes";
         aLangKeys['en']['pp8'] = "8. Contact information";
-      
+
+        aLangKeys['en']['p1'] = "At Virtual Robot, we know your privacy is very important to you, so please take the time to carefully read our privacy policy. Our policy is designed to help you understand the types of information that we collect, how we use and share the information and how the information is protected. This privacy policy applies to all Virtual Robot services that directly reference or link to this policy, but does not apply to Virtual Robot services that have separate privacy policies that do not incorporate this policy.";
+        aLangKeys['en']['p2'] = "Information that you give to us";
+        aLangKeys['en']['p3'] = "When you register for and use our services we collect the information that you give to us. This may include information like your name, email address, phone number, date of birth, country of residence, language, gender, and time zone.";
+        aLangKeys['en']['p4'] = "Information we collect when you use our services";
+        aLangKeys['en']['p5'] = "We also collect and process information about your use of our services. This can include information about your device, your location, your interaction with our services and other Virtual Robot users, your content and your purchases.";
+        aLangKeys['en']['p6'] = "Information about your device";
+        aLangKeys['en']['p7'] = "When you use our services we may collect specific information about your device, and across your devices, such as the product model, serial number, operating system, device settings, device performance, Internet service provider, IP address and other unique identifiers.";
+        aLangKeys['en']['p8'] = "Location information";
+        aLangKeys['en']['p9'] = "With your consent, we may collect and process information about your precise location. When we have your location information, we use it to tailor our services for you and others, like helping you establish friend relationships with other Virtual Robot users or telling your existing friends that you are nearby.";
+        aLangKeys['en']['p10'] = "Information about your use of our services";
+        aLangKeys['en']['p11'] = "We collect certain information about your interaction with our services or with other Virtual Robot users through our services. This could include information about your gameplay, your online status, your service use history, your connections with other Virtual Robot users and the content that you share with them.";
+        aLangKeys['en']['p12'] = "Your content";
+        aLangKeys['en']['p13'] = "Our services may allow you to create, upload or share content such as text, images, audio, video, or other content that you create or is licensed to you. When you use any of our services that include these or other similar capabilities we may collect your content in accordance with our user agreements or terms of use and this policy.";
+        aLangKeys['en']['p14'] = "Your purchases";
+        aLangKeys['en']['p15'] = "Some of our services allow you to make purchases. If you use any of our services to make a purchase, we may collect information about the purchase. This could include payment information, such as your credit card number, account authentication information and contact information like your billing and shipping address.";
+        aLangKeys['en']['p16'] = "We use the information that you give us and the information we collect to deliver, maintain and improve our services. We also use this information to provide you with services that you request, develop new services, suggest personally relevant features, offer you customized content, provide you with tailored advertising, protect Virtual Robot and our users, and investigate and prevent activities that are potentially illegal or that violate our user agreements or terms of use.";
+        aLangKeys['en']['p17'] = "Whenever you contact us, we use the information that you provide to us to help resolve issues you might be having when you use our services. We also use information, such as your email address, to respond to you when you contact us, to let you know about changes to our policies and terms, to let you know about changes or improvements to our services and to inform you about other services that we offer.";
+        aLangKeys['en']['p18'] = `We may use cookies, or other similar technologies, on some of the features of our services. Cookies are small files that are typically downloaded to the browser application on your device. Cookies help us to ensure the safety and smooth functioning of certain features of our services and they also help us to collect information about your user preferences. We sometimes use information collected from cookies to improve your user experience and the quality of our services. For example, by saving your user preferences, we may be able to customize our services in ways that you prefer. You may be able to disable cookies in your browser settings or set your browser to alert you when cookies are being used. If you disable cookies, you may not be able to use all of the features of our services. Some web browsers may transmit "do not track" signals. We currently do not take action in response to these signals.`;
+        aLangKeys['en']['p19'] = `Analytics providers are third-party companies that collect information when you use some of our services. These companies use cookies, web beacons, and similar technologies to collect and analyze crash reports, keep track of what content or ads you view, how long you spend on different pages, how you arrived on a particular page and how you respond to the ads we show you. Analytics providers may combine the information they collect from our services with other information they collect from other sites and services. The practices of these third-party analytics providers are governed by their own privacy policies. Some of these companies, may give users choices about how they collect and use your information.`;
+        aLangKeys['en']['p20'] = `We may share your information for legal and business purposes, such as complying with legal process, responding to claims or inquiries, enforcing our terms, or protecting the rights, property or personal safety of our operations, our users, or the public. Your information may also be shared as part of any sale or transfer of company assets, if legally permitted.
+        If you choose to connect with a third-party service through our services, we may share your information with that service and that service may share your information with us. The use of your information, and the use of cookies, web beacons, or other technologies, by these third-party providers is subject to their own privacy policies.`;
+        aLangKeys['en']['p21'] = `Some of our services allow you to share information and content with others. When you decide to use these services, the information or content that you share, your online status, and your gameplay information may be visible to Virtual Robot and its trusted business partners, other Virtual Robot users, and in some cases the public. Please keep in mind, when you use these service, that the information and content that you choose to share can be read, collected, or used by others. You are fully responsible for the information and content that you choose to share in these instances.`;
+        aLangKeys['en']['p22'] = `We may offer you certain controls and choices regarding the information we collect, how the information is used, and how it is shared. These controls and choices may include the ability to update, correct or delete information that you have provided to us or information that we have collected through your use of our service. They may also include the ability to opt-out of receiving notifications, promotions, offers or other advertising from us.`;
+        aLangKeys['en']['p23'] = "We have implemented administrative, physical and technical security measures that are designed to protect your information from loss, theft, misuse, unauthorized access, disclosure, alteration and destruction. You should understand though that, despite our efforts, no security can be guaranteed as impenetrable.";
+        aLangKeys['en']['p24'] = "We do not knowingly collect, use or share personal information from children under the age of 13 without verifiable parental consent or as permitted by law. Where a parent or legal guardian has provided consent, the child may be able to use his or her Virtual Robot Account to play certain third-party games and applications that are not published by Virtual Robot. Parents or guardians may review, modify, or delete the child’s personal information, or withdraw consent, by contacting us using the contact information provided at the end of this privacy policy, and may manage which third-party games and applications can access and use the child’s Virtual Robot Account information through parental controls.";
+        aLangKeys['en']['p25'] = `Virtual Robot may update this privacy policy from time to time. When we update it, we will revise the "Last Updated" date above. If we make material changes in the way we collect, use, retain or share your personal information, we will notify you by sending you an email at the last email address that you provided us, or by posting notice of the changes on the services covered by this privacy policy.`;
+        aLangKeys['en']['p26'] = `If you have any questions, concerns or comments regarding this privacy policy, or any of our privacy practices, please contact us via email at`;
+        
         const spanis_lc = "es";
         const english_lc = "en";
         curr_lang = spanis_lc;
         var public_ip;
+
     /*ipfi */
     $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
         function (json) {
             public_ip = json.ip;
             console.log(json.ip);
-            
+
             $.getJSON('http://api.ipstack.com/' + public_ip + '?access_key=74eda19312e616de81089ad81ac39014', function (data) {
- 
+
                 var language_code = data.location.languages[0].code;
                 console.log("codigo lenguaje", language_code);
-                
+
                 if (language_code != spanis_lc) {
                     curr_lang = english_lc;
                 }
@@ -176,7 +205,7 @@ $(document).ready(function () {
                 $('.tr').each(function (i) {
                     $(this).text(aLangKeys[curr_lang][$(this).attr('key')]);
                 });
-    
+
             });
         }
 
