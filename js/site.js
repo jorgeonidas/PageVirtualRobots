e15bc7cc7eb06570e4ac4da73b03969dde3e5e3b
Jorge Michele Hernandez Bucci <jhernandez@virtualrobotstud.io>
2018-08-23 14:53:57 -0500

    boton paypal mobile, falta replace con jquery

--- js/site.js
+++ js/site.js
@@ -317,9 +317,9 @@ $(window).resize(function () {
 function replacePpButton(){
    let donateImgEn ="images/English/Donation.png"
     let ppEnglishDesktiop = `
-    <div class="paypal">
-        <img id="donation-img" src="`+donateImgEn+`" style="width: 100%;height: 80%">
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+    <div class="paypal" style="background-color: #f3f3f3;">
+        <img id="donation-img" src="`+donateImgEn+`" style="width: 100%;height: 75%">
+        <form style="padding-top: 0.5rem" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="NU5XZ6R9TML34">
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
